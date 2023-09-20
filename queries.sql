44b13497c5a7515847457ed6ae474a736b819444
Christopher Uduekwe <christopheruduekwe@gmail.com>
2023-09-20 09:12:51 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,40 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT name, species FROM animals LIMIT 5;
+ROLLBACK;
+SELECT name, species FROM animals LIMIT 5;
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT name, species FROM animals LIMIT 5;
+COMMIT;
+SELECT name, species FROM animals LIMIT 5;
+BEGIN;
+DELETE FROM animals;
+SELECT COUNT(*) FROM animals;
+ROLLBACK;
+SELECT COUNT(*) FROM animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
